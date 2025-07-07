0bab7ca4b0ff40b13b0a29e9dbfbddf108b06c2a
christophergyman <christophergayiuman@gmail.com>
2025-07-07 14:51:36 +0100

    feat: window and context working

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,63 @@
+#include <OpenGL/OpenGL.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+
+// user input processing
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// resize viewport when window size changes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}  
 
 int main() {
-    if (!glfwInit()) return -1;
-
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello GLFW", NULL, NULL);
-    if (!window) {
+    // GLFW initial setup
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    // window setup
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Make Game Art Mac", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
+    std::cout << "Window created sucessfully" << std::endl;
+    
 
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
+    // init Glad before open gl calls
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }   
+    
+    // init viewport location
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+    
+    // render loop
+    while(!glfwWindowShouldClose(window))
+    {
         glfwSwapBuffers(window);
-        glfwPollEvents();
+        glfwPollEvents();    
     }
-
-    glfwDestroyWindow(window);
+    // render loop finished
     glfwTerminate();
     return 0;
+
+
 }
